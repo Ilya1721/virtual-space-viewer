0f02901686b378baef6f74a5d7c1d957a91d3358
ilya1721 <ilyavusnya@gmail.com>
2025-03-18 17:39:01 +0200

    feat: now we can connect to the matterport sdk and normally view the model.

--- client/src/app/components/scene.tsx
+++ client/src/app/components/scene.tsx
@@ -1,12 +1,43 @@
-import { useEffect } from "react";
+"use client";
+
+import { useCallback, useEffect, useRef } from "react";
 import styles from "./scene.module.css";
-import { connect } from "../lib/matterportSDK";
+import { MatterportSDK, MP_Window } from "../lib/matterportSDK";
 
 export default function Scene() {
   const apiKey = process.env.MATTERPORT_SDK_KEY;
   const modelSID = process.env.MATTERPORT_MODEL_SID;
   const iframeSrc = `/third_party/matterportSDK/showcase.html?m=${modelSID}&applicationKey=${apiKey}`;
   const iframeId = 'viewer';
+  const mpSDKRef = useRef<MatterportSDK | null>(null);
+
+  const handleIframeLoad = useCallback(async () => {
+    const iframe = document.getElementById(iframeId) as HTMLIFrameElement;
+    if (!iframe) {
+      return;
+    }
+
+    mpSDKRef.current = new MatterportSDK(iframe.contentWindow as MP_Window);
+    const mpSDK = mpSDKRef.current;
+    await mpSDK.connect();
+
+    if (!mpSDK) {
+      return;
+    }
+  }, []);
+
+  useEffect(() => {
+    // Use timer for now, since onLoad for Iframe does not work for some reason
+    const timer = setTimeout(async () => {
+      await handleIframeLoad();
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [handleIframeLoad]);
+
+  if (!apiKey || !modelSID) {
+    return <></>;
+  }
 
   return (
     <div className={styles.scene}>
--- client/src/app/lib/matterportSDK.ts
+++ client/src/app/lib/matterportSDK.ts
@@ -1,8 +1,21 @@
-export function connect(showcaseWindow: Window) {
-  return new Promise((resolve, reject) => {
-    showcaseWindow.MP_SDK.connect().then((mpSdk) => {
-      resolve(mpSdk);
-    }).catch((error) => {
-      reject(error);
-    });
-})};
+export interface MP_SDK
+{
+  connect: () => Promise<void>;
+}
+
+export interface MP_Window extends Window
+{
+  MP_SDK: MP_SDK
+}
+
+export class MatterportSDK {
+  private sdk: MP_SDK;
+
+  public constructor(private window: MP_Window) {
+    this.sdk = this.window.MP_SDK as MP_SDK;
+  }
+
+  public async connect(): Promise<void> {
+    await this.sdk.connect();
+  };
+}
